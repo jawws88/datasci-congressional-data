5255991bec075ee2721e2889f35aaf3fee13bc43
Vijai Narayanan <vijainarayanan@Vijais-MacBook-Pro.local>
2018-03-06 21:47:49 -0800

    Begin deep learning investigation

---
+++ notebooks/sql/campaign_finance_and_election_results_VN.sql
@@ -0,0 +1,30 @@
+with candidate_donations as
+(
+select
+  recipient_candidate_office,
+  regexp_replace(lower(split_part(recipient_candidate_name, ',', 2) || split_part(recipient_candidate_name, ', ', 1)), ' |\.|,|"|-|''|\(|\)', '', 'g') as full_name,
+  sum(transaction_amount) as total_transaction
+from trg_analytics.candidate_contributions
+-- where election_cycle = '2015'
+group by recipient_candidate_name, recipient_candidate_office
+),
+
+election_results as
+(
+select
+  contest_name,
+  regexp_replace(lower(candidate_name), ' |\.|,|"|-|''|\(|\)', '', 'g')  as candidate_name,
+  vote_total,
+  rank() over (partition by contest_name order by vote_total desc) = 1 as is_winner
+from data_ingest.casos__california_candidate_statewide_election_results
+ where county_name = 'State Totals' and contest_name <> 'President' and contest_name <> 'US Senate - 1'
+)
+
+select
+  candidate_name,
+  total_transaction,
+  recipient_candidate_office,
+  is_winner::int as is_winner
+from candidate_donations
+join election_results
+on candidate_donations.full_name = election_results.candidate_name
