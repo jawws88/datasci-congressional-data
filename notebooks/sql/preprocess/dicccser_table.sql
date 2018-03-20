b09cabd3d4ab3e5e121c5813fd33b0c2ea7b4167
philemmons <31429632+philemmons@users.noreply.github.com>
2018-03-19 18:06:35 -0700

    Update dicccser_table.sql
    added where clause to remove Federally elected candidates

--- notebooks/sql/preprocess/dicccser_table.sql
+++ notebooks/sql/preprocess/dicccser_table.sql
@@ -13,6 +13,11 @@ candidate_name -- 40 empty/null
 --vote_total -- 40 empty/null
 
 from data_ingest.casos__california_candidate_statewide_election_results
+where county_name like 'State Totals' 
+  and contest_name not like 'President%' 
+  and contest_name not like 'US Senate%' 
+  and contest_name not like 'United States Representative%'
+
 
 group by
 candidate_name
