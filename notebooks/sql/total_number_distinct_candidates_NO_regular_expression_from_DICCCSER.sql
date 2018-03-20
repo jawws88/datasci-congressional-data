9f6137ed6d127d8e4569696b2fc5c9980966b282
philemmons <31429632+philemmons@users.noreply.github.com>
2018-03-19 19:02:12 -0700

    improved where clause

--- notebooks/sql/total_number_distinct_candidates_NO_regular_expression_from_DICCCSER.sql
+++ notebooks/sql/total_number_distinct_candidates_NO_regular_expression_from_DICCCSER.sql
@@ -5,5 +5,12 @@ from (
   select
     distinct(candidate_name) as name
   from data_ingest.casos__california_candidate_statewide_election_results
-  where county_name = 'State Totals' and contest_name <> 'President' and contest_name <> 'US Senate - 1'
+ where county_name like 'State Totals' 
+    and contest_name not like 'President%'
+    and contest_name not like 'president%'
+    and contest_name not like 'US Senate%' 
+    and contest_name not like 'United States Representative%'
+    and contest_name not like 'us'
+    and contest_name not like 'united%'
+    and contest_name not like '%Congressional District'
 )sub
