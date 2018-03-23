864ca7368ca6f8ebb18c1f26d41af0a3f01997e9
JessDF <jdowding@csumb.edu>
2018-03-23 15:01:30 -0700

    Update LineChart and include package for d3-shapes TODO: For some reason, still not regesturing line as a function of d3 even though included package for it. Package included: https://github.com/d3/d3-shape Resource: http://d3indepth.com/shapes/#line-generator

--- django/congressionaldata/webapp/components/charts/LineChart.jsx
+++ django/congressionaldata/webapp/components/charts/LineChart.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { scaleLinear, scaleBand } from 'd3-scale';
+import { scaleLinear, scaleBand, scaleTime } from 'd3-scale';
 import { max } from 'd3-array'
 import { select, selectAll } from 'd3-selection'
 import { axisBottom, axisLeft } from 'd3-axis'
+import { line } from 'd3-shape'
 
 class LineChart extends Component {
 
@@ -19,56 +20,53 @@ class LineChart extends Component {
         const { width, height, xKey, yKey, barColor, data } = this.props;
         const margin = {top: 20, right: 20, bottom: 30, left: 50};
 
-        const x = scaleTime()
-            .rangeRound([0, width])
-            .padding(0.1)
+        const xScale = scaleLinear()
+	    .domain([0, 20])
+            .range([0, width]);
+            //.padding(0.1)
 
-        const y = scaleLinear()
-            .rangeRound([height, 0])
+        const yScale = scaleLinear()
+	    .domain([0,1])
+            .range([height, 0]);
 			
-		const line = d3.line()
-			.x.domain(data.map(d => d[xKey]))
-			.y.domain([0, max(data, d => +d[yKey])]).nice();
+	const line = line()
+		.x(function(d, i){return xScale(i);})
+		.y(function (d){return yScale(d.y); });
+		//.x.domain(data.map(d => d[xKey]))
+		//.y.domain([0, max(data, d => +d[yKey])]).nice();
 		
-		const g = select(this.svg).append('g');
+	const g = select(this.svg).append('g');
 		
-		g.append("g")
-		    .attr("transform", "translate(0," + height + ")")
-		    .call(d3.axisBottom(x))
-		 .select(".domain")
-		    .remove();
+	g.append("g")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(axisBottom(xScale))
+	 .select(".domain")
+	    .remove();
 
-		g.append("g")
-			.call(d3.axisLeft(y))
-		 .append("text")
-			.attr("fill", "#000")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", "0.71em")
-			.attr("text-anchor", "end")
-			.text("Price ($)");
+	g.append("g")
+		.call(axisLeft(yScale))
+	 .append("text")
+		.attr("fill", "#000")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", "0.71em")
+		.attr("text-anchor", "end")
+		.text("Price ($)");
 
-		g.append("path")
-			.datum(data)
-			.attr("fill", "none")
-			.attr("stroke", "steelblue")
-			.attr("stroke-linejoin", "round")
-			.attr("stroke-linecap", "round")
-			.attr("stroke-width", 1.5)
-			.attr("d", line);
-        g.selectAll()
-            .data(data)
-            .enter().append('rect')
-                .attr('fill', barColor)
-                .attr('x', d => x(d[xKey]))
-                .attr('y', d => y(d[yKey]))
-                .attr('width', x.bandwidth())
-                .attr('height', d => height - margin.bottom - y(d[yKey]))
-				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-    }
-
+	g.append("path")
+		.datum(data)
+		.attr("class", "line")
+		.attr("d", line)
+        g.selectAll(".dot")
+	    .data(dataset)
+	  .enter().append("circle") // Uses the enter().append() method
+	    .attr("class", "dot") // Assign a class for styling
+	    .attr("cx", function(d, i) { return xScale(i) })
+	    .attr("cy", function(d) { return yScale(d.y) })
+	    .attr("r", 5);
+   }
     render() {
-		const { width, height } = this.props;
+	const { width, height } = this.props;
         return <svg ref={node => this.svg = node}
             width={width}
             height={height}>
